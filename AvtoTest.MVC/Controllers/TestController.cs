62ba6f8f0c3789166f38bb108d5b099d1d48956e
fuga002 <159753999+fuga002@users.noreply.github.com>
2024-07-18 22:48:46 +0500

    taking test action was done

--- AvtoTest.MVC/Controllers/TestController.cs
+++ AvtoTest.MVC/Controllers/TestController.cs
@@ -8,6 +8,7 @@ public class TestController : Controller
 {
 
     private readonly TestService _testService;
+    private const string CorrectAnswersCount = "CorrectAnswersCount";
 
     public TestController(TestService testService)
     {
@@ -20,7 +21,31 @@ public class TestController : Controller
             Where(t => t.Id >= ticket.StartIndex &&
                        t.Id <= ticket.EndIndex).
             ToList();
+        ViewBag.TicketId = ticket.Id;
+
+        ViewBag.Context = HttpContext;
+
         return View(tests);
+    }
+
+    [HttpPost]
+    public IActionResult GetTests(byte ticketId = 0, int testId = 0, int choiceId = 0)
+    {
+        int count = GetCorrectAnswersCount();
+       
+        var ticket = new Ticket() { Id = ticketId };
+
+        var test = _testService.Tests.Find(t => t.Id == testId);
+        if (test.Choices[choiceId].Answer)
+        {
+            count++;
+        }
+        if (testId != 0)
+        {
+            AddCookie(testId.ToString(),choiceId.ToString());
+            AddCookie(CorrectAnswersCount,count.ToString());
+        }
+        return RedirectToAction("GetTests", ticket);
     }
 
     public IActionResult Tickets()
@@ -34,6 +59,33 @@ public class TestController : Controller
         var ticket = new Ticket() { Id = id };
         return RedirectToAction("GetTests",ticket);
     }
+
+
+    private void AddCookie(string key, string value)
+    {
+        var check = CheckCookie(key);
+        if (!check)
+        {
+            HttpContext.Response.Cookies.Delete(key);
+        }
+        HttpContext.Response.Cookies.Append(key, value);
+    }
+
+    private bool CheckCookie(string key)
+    {
+        var value = HttpContext.Request.Cookies[key];
+        return string.IsNullOrEmpty(value);
+    }
+
+    private int GetCorrectAnswersCount()
+    {
+        string correctAnswersCount = HttpContext.Request.Cookies[CorrectAnswersCount]!;
+        var count = string.IsNullOrEmpty(correctAnswersCount) ? 0 :
+            Convert.ToInt32(correctAnswersCount);
+        return count;
+    }
+
+
 
 
 }
